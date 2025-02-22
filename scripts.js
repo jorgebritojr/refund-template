a7ee7760c292ad190ca04451e9ea1ac2d42f586b
Jorge Brito <jorgebritojr@gmail.com>
2025-02-22 14:17:45 -0500

    Aceitando somente números no valor 2

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
+// Aceitando somente números no valor 2
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount")
 
 amount.oninput = () => {
-  let value  = amount.value // Amostrar tudo que é colocado no input
-  console.log(value) // console.log para teste
+  let value  = amount.value.replace(/\D/g, "") // Remove qualquer letrar que é digitada no input 
+  console.log(value) // para teste no console
+
 }
