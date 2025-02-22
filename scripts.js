9d68df56794ffebcaaa1818e7205f6b2710ae2ac
Jorge Brito <jorgebritojr@gmail.com>
2025-02-22 14:09:51 -0500

    Capturando o evento de input

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+// Seleciona os elementos do formulário
+const amount = document.getElementById("amount")
 
+// O "oninput" observa toda vez que entra conteúdo no input
+amount.oninput = () => {
+ // console.log("Novo conteúdo") // comando para testar o campo input do html
+}
