5f2bc95c41b698396052383de10d9038fa9f1c28
Jorge Brito <jorgebritojr@gmail.com>
2025-02-22 14:19:24 -0500

    Aceitando somente números no valor 3

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-// Aceitando somente números no valor 2
+// Aceitando somente números no valor 3
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount")
 
 amount.oninput = () => {
-  let value  = amount.value.replace(/\D/g, "") // Remove qualquer letrar que é digitada no input 
-  console.log(value) // para teste no console
+  let value  = amount.value.replace(/\D/g, "") // Removendo as letras
+  amount.value = value // Colocando no input somente os números
 
 }
