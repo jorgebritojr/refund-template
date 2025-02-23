3f1b31ea7cbd1012ad7721b5757be64988e42f1b
Jorge Brito <jorgebritojr@gmail.com>
2025-02-23 11:26:37 -0500

    Criando o ícone de remover

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Criando o valor da despesa
+// Criando o ícone de remover
 
 // Seleciona os elementos do formulário.
 const form = document.querySelector("form")
@@ -82,10 +82,17 @@ function expenseAdd(newExpense) {
          .toUpperCase()
          .replace("R$", "")}`
   
+    
+  
+    //Cria o ícone de remover
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src", "img/remove.svg")
+    removeIcon.setAttribute("alt", "remover")
 
 
     // Adiciona as informações no item.
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
     // Adiciona o item na lista.
     expenseList.append(expenseItem)
